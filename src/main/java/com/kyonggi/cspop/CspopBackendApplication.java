a8f5e47a69baf82d5428235a7223a77c6d134103
SangBeom-Hahn <hsb990917@gmail.com>
2024-08-13 22:34:47 +0900

    Initial commit

---
+++ src/main/java/com/kyonggi/cspop/CspopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kyonggi.cspop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CspopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CspopBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyonggi/cspop/CspopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyonggi.cspop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CspopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
