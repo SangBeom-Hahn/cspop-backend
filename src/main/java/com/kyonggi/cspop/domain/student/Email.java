730ee5db796ebcdfed0a134252ce9a3d336e5590
SangBeom-Hahn <hsb990917@gmail.com>
2024-08-14 15:41:25 +0900

    feat: Email 기능 및 테스트 구현
    #5

---
+++ src/main/java/com/kyonggi/cspop/domain/student/Email.java
@@ -0,0 +1,37 @@
+package com.kyonggi.cspop.domain.student;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Getter
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Email {
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$");
+
+    @Column(name = "email", nullable = false, length = 30)
+    private String value;
+
+    private Email(String value) {
+        this.value = value;
+    }
+
+    public static Email from(String value) {
+        validatePattern(value);
+        return new Email(value);
+    }
+
+    private static void validatePattern(String value) {
+        Matcher matcher = EMAIL_PATTERN.matcher(value);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
