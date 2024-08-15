3597757d4ac0323e188236355f28fed90e3e23a0
SangBeom-Hahn <hsb990917@gmail.com>
2024-08-15 23:46:27 +0900

    chore: 더미 데이터 생성 방식을 init.sql로 변경
    #13

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,6 @@
+insert into student
+    (birth, classification, created_date, department, email, grade, last_modified_date, login_id, student_name, student_number, password, phone_number, role_type)
+values ('1999-09-17','UNDERGRADUATE_STUDENT',NOW(),'AI','1@naver.com','FIRTH',
+        NOW(),'123','dummy','201811111','$2a$10$oXGskC./TduQr2ngwoAqg.RX6cU0Hi.d17DxIlY0bnJKeb/qSxQqu',
+        '010-1111-1111','STUDENT');
+
