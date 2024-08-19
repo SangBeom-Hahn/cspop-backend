615c5560674186f5a304b85623f2579265ddb35d
SangBeom-Hahn <hsb990917@gmail.com>
2024-08-19 16:52:02 +0900

    feat: GraduationController 기능 구현
    #10

--- src/main/java/com/kyonggi/cspop/controller/GraduationController.java
+++ src/main/java/com/kyonggi/cspop/controller/GraduationController.java
@@ -3,15 +3,14 @@ package com.kyonggi.cspop.controller;
 import com.kyonggi.cspop.controller.auth.AuthenticationPrincipal;
 import com.kyonggi.cspop.controller.dto.graduate.GraduationSaveRequest;
 import com.kyonggi.cspop.controller.dto.student.LoginStudentRequest;
+import com.kyonggi.cspop.service.dto.graduate.GraduationResponseDto;
 import com.kyonggi.cspop.service.dto.graduate.GraduationSaveResponseDto;
+import com.kyonggi.cspop.service.dto.graduate.GraduationsResponseDto;
 import com.kyonggi.cspop.service.graduate.GraduationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -34,5 +33,15 @@ public class GraduationController {
         return ResponseEntity
                 .created(URI.create("/api/graduates/" + graduationSaveResponseDto.getId()))
                 .body(graduationSaveResponseDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<GraduationsResponseDto> findGraduations() {
+        return ResponseEntity.ok(graduationService.findAllGraduation());
+    }
+
+    @GetMapping("/{studentId}")
+    public ResponseEntity<GraduationResponseDto> findGraduation(@PathVariable("studentId") Long studentId) {
+        return ResponseEntity.ok(graduationService.findGraduation(studentId));
     }
 }
