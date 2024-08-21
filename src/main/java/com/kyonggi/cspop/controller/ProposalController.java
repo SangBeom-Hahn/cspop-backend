05178e710faf0453bd2a3d7945d6eda100f0c998
SangBeom-Hahn <hsb990917@gmail.com>
2024-08-21 21:40:26 +0900

    feat: ProposalController 기능 구현
    #10

---
+++ src/main/java/com/kyonggi/cspop/controller/ProposalController.java
@@ -0,0 +1,46 @@
+package com.kyonggi.cspop.controller;
+
+import com.kyonggi.cspop.controller.dto.graduate.RejectRequest;
+import com.kyonggi.cspop.controller.dto.graduate.ProposalSaveRequest;
+import com.kyonggi.cspop.service.dto.graduate.proposal.ProposalSaveResponseDto;
+import com.kyonggi.cspop.service.graduate.ProposalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/proposals")
+public class ProposalController {
+
+    private final ProposalService proposalService;
+
+    @PostMapping("/{studentId}")
+    public ResponseEntity<ProposalSaveResponseDto> save(
+            @PathVariable("studentId") Long studentId,
+            @RequestBody @Validated ProposalSaveRequest proposalSaveRequest
+    ) {
+        ProposalSaveResponseDto proposalSaveResponseDto = proposalService.saveProposal(proposalSaveRequest.toServiceDto(), studentId);
+        return ResponseEntity
+                .created(URI.create("/api/proposals/" + proposalSaveResponseDto.getId()))
+                .body(proposalSaveResponseDto);
+    }
+
+    @PutMapping("/{studentId}/approve")
+    public ResponseEntity<Void> approve(@PathVariable("studentId") Long studentId) {
+        proposalService.approveProposal(studentId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{studentId}/reject")
+    public ResponseEntity<Void> reject(
+            @PathVariable("studentId") Long studentId,
+            @RequestBody @Validated RejectRequest rejectRequest
+    ) {
+        proposalService.rejectProposal(studentId, rejectRequest.getReason());
+        return ResponseEntity.noContent().build();
+    }
+}
