09b9b990f092c20bc3095a78213b822da8986109
SangBeom-Hahn <hsb990917@gmail.com>
2024-08-22 16:04:07 +0900

    feat: GuideBoard 기능 및 테스트 구현
    #9

---
+++ src/main/java/com/kyonggi/cspop/domain/GuideBoard.java
@@ -0,0 +1,30 @@
+package com.kyonggi.cspop.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@ToString
+@Table(name = "guide_board")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GuideBoard extends BaseEntity{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "guide_board_id")
+    private Long id;
+
+    @Column(name = "content", nullable = false, length = 15000)
+    private String content;
+
+    public GuideBoard(String content) {
+        this.content = content;
+    }
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
+}
