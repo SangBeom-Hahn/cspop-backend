dcb4a98f9a7e68e5ec8ecf4c122b75bda25b1b64
SangBeom-Hahn <hsb990917@gmail.com>
2024-08-22 16:04:26 +0900

    feat: GuideBoardRepository 기능 및 테스트 구현
    #9

---
+++ src/main/java/com/kyonggi/cspop/repository/GuideBoardRepository.java
@@ -0,0 +1,7 @@
+package com.kyonggi.cspop.repository;
+
+import com.kyonggi.cspop.domain.GuideBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuideBoardRepository extends JpaRepository<GuideBoard, Long> {
+}
